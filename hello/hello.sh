a4e4d053423028d750d81316265f68c2928c03dc
Tobias Johansson <tobiajo@kth.se>
2016-05-12 08:06:00 +0200

    Prettify shell script output

--- hello/hello.sh
+++ hello/hello.sh
@@ -1,5 +1,12 @@
 make > /dev/null 2>&1
+
+echo '> sudo insmod hello.ko'
 sudo insmod hello.ko
+echo '> dmesg | tail -1'
+dmesg | tail -1
+echo '> sudo rmmod hello'
 sudo rmmod hello
-dmesg | tail
+echo '> dmesg | tail -1'
+dmesg | tail -1
+
 make clean > /dev/null 2>&1
--- rootkit/rootkit.sh
+++ rootkit/rootkit.sh
@@ -1,12 +1,22 @@
 make > /dev/null 2>&1
+
+echo '> sudo insmod rootkit.ko'
 sudo insmod rootkit.ko
+echo '> dmesg | tail -1'
+dmesg | tail -1
 
 echo '> whoami'
 whoami
 echo '> kill 5'
 kill 5
+echo '> dmesg | tail -1'
+dmesg | tail -1
 echo '> whoami'
 whoami
 
+echo '> sudo rmmod rootkit'
 sudo rmmod rootkit
+echo '> dmesg | tail -1'
+dmesg | tail -1
+
 make clean > /dev/null 2>&1
